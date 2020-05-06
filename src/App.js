4720a4692472ab677821f40574e729d1f8b65cd3
Matt Kam <matt.ph.kam@gmail.com>
2020-05-05 19:51:33 -0700

    added emoji ranking system using aws lambda

--- src/App.js
+++ src/App.js
@@ -157,7 +157,8 @@ class App extends Component {
             method: 'put',
             headers: {
               'Content-Type': 'application/json',
-              'Authorization': window.sessionStorage.getItem('token')
+              'Authorization': window.sessionStorage.getItem('token'),
+              'Accept': 'application/json'
             },
             body: JSON.stringify({
               id: this.state.user.id
